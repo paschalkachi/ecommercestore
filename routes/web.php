b7930d0233dc205407f2a1fa1846b74be000c799
paschalkachi <paschalnnamani10@gmail.com>
2025-10-29 03:39:56 +0100

    added user type column to users table

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,6 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
-
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
