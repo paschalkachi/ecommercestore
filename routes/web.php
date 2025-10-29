1c3f71b522f30fb7566160a57e9d286fdea6f63e
paschalkachi <paschalnnamani10@gmail.com>
2025-10-29 03:31:06 +0100

    Added a new folder, assets inside the public folder

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -18,3 +18,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/',[HomeController::class,'index'])->name('home.index');
