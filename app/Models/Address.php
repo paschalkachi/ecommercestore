df94d3fb0c41c221b7a4925e7c987a202e34535d
Paschal <paschalnnamani10@gmail.com>
2025-12-15 06:50:42 +0100

    Added protected fillables to address model

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'name',
+        'phone',
+        'zip',
+        'state',
+        'city',
+        'address',
+        'locality',
+        'landmark',
+        'country',
+        'isdefault',
+    ];
+
+    protected $casts = [
+        'isdefault' => 'boolean',
+    ];
 }
